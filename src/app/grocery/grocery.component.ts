8257ac136d469b4a3a0452d6bc7a98b717ddf805
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2021-10-12 22:31:44 -0700

    Added functionality and methods

--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Grocery } from '../models/grocery-models';
 
 @Component({
   selector: 'app-grocery',
@@ -6,12 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grocery.component.css',]
 })
 export class GroceryComponent implements OnInit {
-    // task: string;
-    tasks = [];
+
+  groceries:Grocery[];
+
+  inputGroceries = "";
   
   constructor() { }
 
   ngOnInit(): void {
+
+    this.groceries = [
+      {
+        content: 'First item',
+        completed: false
+      },
+      {
+        content: 'Second item',
+        completed: false
+      }
+    ]
+  }
+
+  toggleDone (id: number) {
+    this.groceries.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+  deleteItem (id:number) {
+    this.groceries = this.groceries.filter((v, i) => i !==id);
+
+  }
+
+  addItem () {
+    this.groceries.push({
+      content: this.inputGroceries,
+      completed: false
+    });
+
+    this.inputGroceries = "";
   }
 
 }
